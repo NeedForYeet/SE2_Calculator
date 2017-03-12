8032fc85f1b9083f35ea09b054fae00f0d498a99
walterje <walterje@edu.aau.at>
2017-03-13 00:24:18 +0100

    OnClickListener for calculate Button and declaration of calculateResult() method

--- app/src/main/java/at/walterje/calculator/MainActivity.java
+++ app/src/main/java/at/walterje/calculator/MainActivity.java
@@ -2,6 +2,8 @@ package at.walterje.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set OnClickListener for the calculate button
+        Button calcButton = (Button) findViewById(R.id.buttonCalculate);
+        calcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateResult();
+            }
+        });
+    }
+
+    /** Calculate the result of both entered values
+     * and print them on the screen.
+     */
+    private void calculateResult() {
+
     }
 }
